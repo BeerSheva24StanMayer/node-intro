6a47078a56579cfc6bf566885e3cb3c26a0bb975
Stan Mayer <sthumor@gmail.com>
2025-01-26 10:28:17 +0200

    refs_final

---
+++ functions.js
@@ -0,0 +1,5 @@
+export function myBind(thisArg, ...otherArgs) {
+    //TODO
+    //write method myBind fully running standard "bind" method functionality
+    //no using standard call / apply methods
+}
