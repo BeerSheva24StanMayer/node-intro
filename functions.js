8b240bf94cb11bf2b13b7ec5906b1b352ac60b60
Stan Mayer <sthumor@gmail.com>
2025-01-26 11:59:02 +0200

    objects_init

--- functions.js
+++ functions.js
@@ -2,4 +2,11 @@ export function myBind(thisArg, ...otherArgs) {
     //TODO
     //write method myBind fully running standard "bind" method functionality
     //no using standard call / apply methods
+    const fieldTime = new Date().getDate() + "";
+    thisArg[fieldTime] = this;
+    return (...args) => {
+        const res = thisArg(...args, ...bindArgs);
+        delete thisArg[fieldTime];
+        return res;
+    }
 }
